fb963b6cc74bf6762e0c48d4c4321647ae20f4f8
Xabi_U <xabierurrutia2004@gmail.com>
2026-05-21 03:09:36 +0200

    New version

--- app/config.py
+++ app/config.py
@@ -1,11 +1,10 @@
 import os
 
-GAME_VERSION   = "1.0.0"
+GAME_VERSION   = "1.1.0"
 INSTALLER_WIN  = "Reconquest_Setup_v1.0.0.exe"
 INSTALLER_LNX  = "Reconquest_Setup_v1.0.0.sh"
-INSTALLER_URL_WIN = "https://github.com/XabierUrrutia/Renconquest_Web/releases/download/v1.0.0/Reconquest_Setup_v1.0.0.exe"
-INSTALLER_URL_LNX = "https://github.com/XabierUrrutia/Renconquest_Web/releases/download/v1.0.0/Reconquest_Setup_v1.0.0.sh"
-
+INSTALLER_URL_WIN = "https://github.com/XabierUrrutia/Reconquest_Web/releases/download/v1.1.0/Reconquest_Setup_v1.1.0.exe"
+INSTALLER_URL_LNX = "https://github.com/XabierUrrutia/Reconquest_Web/releases/download/v1.1.0/Reconquest_Setup_v1.1.0.sh"
 INSTALLER_NAME = INSTALLER_WIN
 INSTALLER_URL  = INSTALLER_URL_WIN
 
